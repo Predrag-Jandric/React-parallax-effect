2e097b5e23b508ecbb541359eb53b5b9b5622149
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-01 23:44:34 +0200

    project content and logic complete, to be deployer and readme updated

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import React from "react";
+import MultiLayerParallax from "./components/MultiLayerParallax"
+import Text from "./components/Text"
 
 function App() {
   return (
-    <main className="App">
-      {/* comp */}
-      <div className="w-full h-full bg-[#06141d]">
-      <div className="max-w-xl space-y-4 mx-auto py-24 px-5 text-neutral-200 text-xl">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
-            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
-            sit dolores odio excepturi facilis laboriosam officiis dolorem,
-            nobis reprehenderit molestiae.
-          </p>
-        </div>
-      </div>
-      
+    <main className="bg-[#06141d]">
+
+      <MultiLayerParallax/>
+      <Text/>
+
     </main>
   );
 }
---
+++ src/components/Text.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Text() {
+    return (
+        <section className="max-w-xl space-y-4 mx-auto py-24 px-5 text-neutral-200 text-xl">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+                earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+                sit dolores odio excepturi facilis laboriosam officiis dolorem,
+                nobis reprehenderit molestiae.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+                earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+                sit dolores odio excepturi facilis laboriosam officiis dolorem,
+                nobis reprehenderit molestiae.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+                earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+                sit dolores odio excepturi facilis laboriosam officiis dolorem,
+                nobis reprehenderit molestiae.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+                earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+                sit dolores odio excepturi facilis laboriosam officiis dolorem,
+                nobis reprehenderit molestiae.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+                earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+                sit dolores odio excepturi facilis laboriosam officiis dolorem,
+                nobis reprehenderit molestiae.
+            </p>
+        </section>
+    )
+}
+
+export default Text
